3dc37e48ab95261f311f370cf2a1ac420678480b
Hubert Lee <hub12345@ymail.com>
2017-03-30 23:18:23 +0800

    First commit

---
+++ Assets/Scripts/MoneyContrller.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyContrller : MonoBehaviour {
+    public Text moneyText;
+    public GameObject serverRequestOBJ;
+    public GameObject buttonOBJ;
+    public class RecievedData
+    {
+        public string money;
+    }
+    private ServerRequest serverRequest;
+    private Button buyButton;
+    private int mMoney;
+	// Use this for initialization
+	void Start () {
+        //Setup components
+        serverRequest = serverRequestOBJ.GetComponent<ServerRequest>();
+        buyButton = buttonOBJ.GetComponent<Button>();
+        buyButton.onClick.AddListener(ButtonOnclick);
+        //buyButton.interactable = false;
+
+        //Read money from server
+        UpdateMoney();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        /*if(mMoney >= 10)
+        {
+            buyButton.interactable = true;
+        }
+        else
+        {
+            buyButton.interactable = false;
+        }*/
+	}
+    void ButtonOnclick()
+    {
+        /*mMoney -= 10;
+
+        //Sub money from server
+        SubMoney(10);
+
+        //Read money from server
+        UpdateMoney();*/
+
+        UpdateMoney();
+
+    }
+
+    void UpdateMoney()
+    {
+        StartCoroutine(serverRequest.DownMoney((returnValue) => {
+            //mMoney = returnValue;
+            moneyText.text = returnValue;
+        }));
+    }
+
+    void AddMoney()
+    {
+        StartCoroutine(serverRequest.AddMoney());
+    }
+
+    void SubMoney(int amount)
+    {
+        StartCoroutine(serverRequest.SubMoney(amount));
+    }
+}
---
+++ Assets/Scripts/ServerRequest.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerRequest : MonoBehaviour {
+
+    private string cookie = "__test=f4a6cec44cd3e9ce11d8a04659bf3a3f;expires=N/A;path=N/A";
+    private string disURL = "http://hub12345.yabi.me/";
+
+    public class RecievedData
+    {
+        public int money;
+    }
+
+    public IEnumerator DownMoney(System.Action<string> callback)
+    {
+        //set content
+        WWWForm form = new WWWForm();
+        form.AddField("user", "rabbit");
+
+        //set headers
+        Dictionary<string, string> headers = form.headers;
+        headers["Cookie"] = cookie;
+
+        //Send post
+        WWW www = new WWW(disURL + "down_money.php", form.data, headers);
+        yield return www;
+
+        if (www.error != null)
+        {
+            Debug.Log(www.error);
+            callback(www.error);
+        }
+        else
+        {
+            RecievedData tempData = JsonUtility.FromJson<RecievedData>(www.text);
+            Debug.Log("DownMoney Success with money = " + tempData.money);
+            callback("success");
+        }
+    }
+
+    public IEnumerator AddMoney()
+    {
+        //set content
+        WWWForm form = new WWWForm();
+        form.AddField("user", "rabbit");
+
+        //set headers
+        Dictionary<string, string> headers = form.headers;
+        headers["Cookie"] = cookie;
+
+        //Send post
+        WWW www = new WWW(disURL + "add_money.php", form.data, headers);
+        yield return www;
+
+        if (www.error != null)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Add money success!!");
+        }
+    }
+
+    public IEnumerator SubMoney(int amount)
+    {
+        //set content
+        WWWForm form = new WWWForm();
+        form.AddField("amount", amount);
+
+        //set headers
+        Dictionary<string, string> headers = form.headers;
+        headers["Cookie"] = cookie;
+
+        //Send post
+        WWW www = new WWW(disURL + "sub_money.php", form.data, headers);
+        yield return www;
+
+        if (www.error != null)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Sub " + amount + " money success!!");
+        }
+    }
+}
